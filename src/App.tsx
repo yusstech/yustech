21691ea4173efd342bd90bf9406c4af0ec05515f
yusstech <yusstechh@gmail.com>
2025-04-08 08:27:04 +0100

    chore: Remove Turnstile captcha and all related configurations

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import TurnstileChallenge from "./components/TurnstileChallenge";
 
 // Create a client
 const queryClient = new QueryClient();
@@ -20,7 +19,6 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <TurnstileChallenge />
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
